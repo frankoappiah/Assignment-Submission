a3b61086c5fc55c1fc9f5986aa0df046aa8583ab
Frank Appiah <102674861+frankoappiah@users.noreply.github.com>
2022-04-04 10:39:06 +0000

    Assignment 1

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+// You are to write a simple C++ code to find the area of a triangle.
+
+int main()
+{
+    double h;
+    double b;
+    
+    cout << "The height: ";
+    cin >> h;
+    
+    cout << "The base: ";
+    cin >> b;
+    
+    double a = 0.5 * b * a;
+    
+    cout << "The area of the triangle is " << a;
+
+    return 0;
+}
